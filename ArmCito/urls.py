18641977d7f438049d44c22cc765f0ef586919bc
go-arnold <arnoldciku@gmail.com>
2024-07-29 15:57:33 +0200

    bien4

--- ArmCito/urls.py
+++ ArmCito/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('',include('students_admins.urls')),
 ]
 
-#urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_header="ULK CONTROL SERVICE"
 admin.site.site_title="ULK- UPI"
